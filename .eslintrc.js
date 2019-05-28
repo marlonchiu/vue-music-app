cf3e8b61c2d1258fa54a54da7862e0ec4af23b59
marlonchiu <Knight1898@outlook.com>
2019-05-28 14:28:46 +0800

    old dev clone

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  env: {
+    browser: true,
+  },
+  extends: [
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    'plugin:vue/essential',
+    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+    'standard'
+  ],
+  // required to lint *.vue files
+  plugins: [
+    'vue'
+  ],
+  // add your custom rules here
+  rules: {
+    /**
+     *  "off" 或 0 - 关闭规则
+     *  "warn" 或 1 - 开启规则，使用警告级别的错误：warn (不会导致程序退出),
+     *  "error" 或 2 - 开启规则，使用错误级别的错误：error (当被触发的时候，程序会退出)
+     */
+    // allow async-await
+    'generator-star-spacing': 'off',
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "eol-last": 0, //文件以单一的换行符结束
+    "space-before-function-paren": [0, "always"], //函数定义时括号前面要不要有空格
+    "indent": ["off", 2],  // 旨在强制使用一致的缩进风格，默认是4个
+    "no-trailing-spaces": 'off',  // //一行结束后面可要不要有空格
+    "quotes": ['off', "single", { "avoidEscape": true }],
+    "no-multi-spaces": ['off']
+  }
+}
