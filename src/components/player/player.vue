cc2e1e50b62eb8e097761425b46f853f7e58b5cd
MarlonChiu <knight1898@outlook.com>
2019-08-17 11:40:25 +0800

    歌单列表 playlist 静态布局

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -93,6 +93,7 @@
         </div>
       </div>
     </transition>
+    <playlist></playlist>
     <audio ref="audio" :src="currentSong.url"
            @play="ready" @error="error" @timeupdate="updateTime" @ended="end"
     ></audio>
@@ -109,6 +110,7 @@
   import {shuffle} from 'common/js/util'
   import Lyric from 'lyric-parser'
   import Scroll from 'base/scroll/scroll'
+  import Playlist from 'components/playlist/playlist'
 
   const transform = prefixStyle('transform')
   const transitionDuration = prefixStyle('transitionDuration')
@@ -441,7 +443,8 @@
     components: {
       Scroll,
       ProgressBar,
-      ProgressCircle
+      ProgressCircle,
+      Playlist
     },
     watch: {
       currentSong(newSong, oldSong) {
