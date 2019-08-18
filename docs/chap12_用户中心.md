abe0068a88d9c2654f4293b6600f3f934b93392b
MarlonChiu <knight1898@outlook.com>
2019-08-18 19:01:14 +0800

    user-center 其他小功能优化

--- docs/chap12_用户中心.md
+++ docs/chap12_用户中心.md
@@ -1,3 +1,10 @@
 ## 用户中心
 
-1）
+1）插入歌曲
+```javascript
+import Song from 'common/js/song'
+
+selectSong(song) {
+  this.insertSong(new Song(song))
+},
+```
--- src/components/user-center/user-center.vue
+++ src/components/user-center/user-center.vue
@@ -8,23 +8,24 @@
         <switches @switch="switchItem"
                   :switches="switches" :currentIndex="currentIndex"></switches>
       </div>
-      <div ref="playBtn" class="play-btn">
+      <div ref="playBtn" class="play-btn" @click="random">
         <i class="icon-play"></i>
         <span class="text">随机播放全部</span>
       </div>
       <div class="list-wrapper" ref="listWrapper">
         <scroll ref="favoriteList" v-if="currentIndex === 0" class="list-scroll" :data="favoriteList">
           <div class="list-inner">
-            <song-list :songs="favoriteList" @select="selectSong">
-            </song-list>
+            <song-list :songs="favoriteList" @select="selectSong"></song-list>
           </div>
         </scroll>
         <scroll ref="playList" v-if="currentIndex === 1" class="list-scroll" :data="playHistory">
           <div class="list-inner">
-            <song-list :songs="playHistory" @select="selectSong">
-            </song-list>
+            <song-list :songs="playHistory" @select="selectSong"></song-list>
           </div>
         </scroll>
+      </div>
+      <div class="no-result-wrapper" v-show="noResult">
+        <no-result :title="noResultDesc"></no-result>
       </div>
     </div>
   </transition>
@@ -35,11 +36,12 @@
   import Switches from 'base/switches/switches'
   import Scroll from 'base/scroll/scroll'
   import SongList from 'base/song-list/song-list'
-  // import NoResult from 'base/no-result/no-result'
+  import NoResult from 'base/no-result/no-result'
   import Song from 'common/js/song'
-  // import {playlistMixin} from 'common/js/mixin'
+  import {playlistMixin} from 'common/js/mixin'
 
   export default {
+    mixins: [playlistMixin],
     data() {
       return {
         currentIndex: 0,
@@ -54,12 +56,32 @@
       }
     },
     computed: {
+      noResult() {
+        if (this.currentIndex === 0) {
+          return !this.favoriteList.length
+        } else {
+          return !this.playHistory.length
+        }
+      },
+      noResultDesc() {
+        if (this.currentIndex === 0) {
+          return '暂无收藏歌曲'
+        } else {
+          return '你还没有听过歌曲'
+        }
+      },
       ...mapGetters([
         'favoriteList',
         'playHistory'
       ])
     },
     methods: {
+      handlePlaylist(playlist) {
+        const bottom = playlist.length > 0 ? '60px' : ''
+        this.$refs.listWrapper.style.bottom = bottom
+        this.$refs.favoriteList && this.$refs.favoriteList.refresh()
+        this.$refs.playList && this.$refs.playList.refresh()
+      },
       back() {
         this.$router.back()
       },
@@ -69,15 +91,29 @@
       selectSong(song) {
         this.insertSong(new Song(song))
       },
+      random() {
+        let list = this.currentIndex === 0 ? this.favoriteList : this.playHistory
+        if (list.length === 0) {
+          return
+        }
+        // 存储的数据不是Song 的实例
+        list = list.map((song) => {
+          return new Song(song)
+        })
+        this.randomPlay({
+          list
+        })
+      },
       ...mapActions([
-        'insertSong'
+        'insertSong',
+        'randomPlay'
       ])
     },
     components: {
       Switches,
       Scroll,
-      SongList
-      // NoResult
+      SongList,
+      NoResult
     }
   }
 </script>
